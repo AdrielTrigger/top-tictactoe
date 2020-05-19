0a4f38f820c64b3ca5c4d069e80bb523b3e56d7a
Adriel <adril_trigger@yahoo.com>
2020-05-18 21:32:14 -0300

    tie properly resolved, win-check logic shortened and some aesthetical 'flair' added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const game = (() => {
     let player1score = document.querySelector('.player1 p');
     let player2score = document.querySelector('.player2 p');
     let newBoard = document.querySelectorAll('.space');
+    let turns = 0;
 
     player1score.textContent = scores.player1;
     player2score.textContent = scores.player2;
@@ -51,82 +52,51 @@ const game = (() => {
                     break;
             }
         }
+        turns++;
         winCheck();
     }
 
     const winCheck = () => {
-        console.log(board);
-        if (board[0][0] == board[0][1] && board[0][1] == board[0][2]) {
-            if (board[0][0] == 'X') {
-                alert('X wins');
-                scores.player1 += 1;
-            } else if (board[0][0] == 'O') {
-                alert('O wins');
-                scores.player2 += 1;
-            }
+        let conditions =    [[board[0][0],board[0][1],board[0][2]],
+                            [board[1][0],board[1][1],board[1][2]],
+                            [board[2][0],board[2][1],board[2][2]],
+                            [board[0][0],board[1][0],board[2][0]],
+                            [board[0][1],board[1][1],board[2][1]],
+                            [board[0][2],board[1][2],board[2][2]],
+                            [board[0][0],board[1][1],board[2][2]],
+                            [board[0][2],board[1][1],board[2][0]]];
+        
+        let a, b, c, win;
+
+        for (con = 0; con < 8; con++) { // con stands for condition
+            win = false;
+            a = conditions[con][0];
+            b = conditions[con][1];
+            c = conditions[con][2];
+            if (a == b && b == c) {
+                if (a == 'X') {
+                    scores.player1++;
+                    player1score.textContent = scores.player1;
+                    alert ('X wins');
+                    win = true;
+                    boardClear();
+                    turns = 0;
+                    break;
+                } else if (a == 'O') {
+                    scores.player2++;
+                    player2score.textContent = scores.player2;
+                    alert ('O wins');
+                    win = true;
+                    boardClear();
+                    turns = 0;
+                    break;
+                }
+            } 
+        }
+        if (turns == 9 && win == false) {
+            alert('It\'s a tie');
             boardClear();
-        } else if (board[1][0] == board[1][1] && board[1][1] == board[1][2]) {
-            if (board[1][0] == 'X') {
-                alert('X wins');
-                scores.player1 += 1;
-            } else if (board[1][0] == 'O') {
-                alert('O wins');
-                scores.player2 += 1;
-            }
-            boardClear();
-        } else if (board[2][0] == board[2][1] && board[2][1] == board[2][2]) {
-            if (board[2][0] == 'X') {
-                alert('X wins');
-                scores.player1 += 1;
-            } else if (board[2][0] == 'O') {
-                alert('O wins');
-                scores.player2 += 1;
-            }
-            boardClear();
-        } else if (board[0][0] == board[1][0] && board[1][0] == board[2][0]) {
-            if (board[0][0] == 'X') {
-                alert('X wins');
-                scores.player1 += 1;
-            } else if (board[0][0] == 'O') {
-                alert('O wins');
-                scores.player2 += 1;
-            }
-            boardClear();
-        } else if (board[0][1] == board[1][1] && board[1][1] == board[2][1]) {
-            if (board[0][1] == 'X') {
-                alert('X wins');
-                scores.player1 += 1;
-            } else if (board[0][1] == 'O') {
-                alert('O wins');
-                scores.player2 += 1;
-            }
-            boardClear();
-        } else if (board[0][2] == board[1][2] && board[1][2] == board[2][2]) {
-            if (board[0][2] == 'X') {
-                alert('X wins');
-                scores.player1 += 1;
-            } else if (board[0][2] == 'O') {
-                alert('O wins');
-                scores.player2 += 1;
-            }
-            boardClear();
-        } else if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-            if (board[0][0] == 'X') {
-                alert('X wins');
-                scores.player1 += 1;
-            } else if (board[0][0] == 'O') {
-                alert('O wins');
-                scores.player2 += 1;
-            }
-            boardClear();
-        } else if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {                if (board[0][2] == 'X') {
-                alert('X wins');
-                scores.player1 += 1;
-            } else if (board[0][2] == 'O') {
-                alert('O wins');
-                scores.player2 += 1;
-            }
-            boardClear();
+            turns = 0;
         }
     }
 
@@ -135,8 +105,6 @@ const game = (() => {
             newBoard[i].textContent = '';
         }
         board = [[1,2,3],[4,5,6],[7,8,9]];
-        player1score.textContent = scores.player1;
-        player2score.textContent = scores.player2;
     }
         
 
@@ -162,6 +130,25 @@ function getEventTarget(e) {
     return e.target;
 }
 
+let player1colors = document.querySelector('.player1');
+let player2colors = document.querySelector('.player2');
+
+function showPriority(priority) {
+    if (priority == player1) {
+        player1colors.style.setProperty('background-color', 'rgb(138, 20, 20)');
+        player1colors.style.setProperty('color', 'white');
+        player2colors.style.setProperty('background-color', 'white');
+        player2colors.style.setProperty('color', 'rgb(23, 23, 163)');
+    } else {
+        player1colors.style.setProperty('background-color', 'white');
+        player1colors.style.setProperty('color', 'rgb(138, 20, 20)');
+        player2colors.style.setProperty('background-color', 'rgb(23, 23, 163)');
+        player2colors.style.setProperty('color', 'white');
+    }
+}
+
+showPriority(priority);
+
 htmlBoard.addEventListener('click', (e) => { 
     game.markBoard(priority,e);
     let mark = getEventTarget(e).textContent;
@@ -172,4 +159,5 @@ htmlBoard.addEventListener('click', (e) => {
     } else {
         game.boardClear();
     }
+    showPriority(priority);
 });
